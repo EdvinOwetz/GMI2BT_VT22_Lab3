fee4e09ff14dc4acf338869c945ee181d5ed58b7
EdvinOwetz <edvinowetz@gmail.com>
2022-02-02 12:11:46 +0100

    Started building a catalog-tree, tested the api-results and a few bits and bobs.
    Co-authored-by: Nisslett <Nisslett@users.noreply.github.com>

---
+++ historyentry.py
@@ -0,0 +1,4 @@
+
+
+class HistoryEntry:
+    pass
---
+++ menu.py
@@ -0,0 +1,10 @@
+
+
+class Menu:
+    def __init__(self):
+        # ARBETSLISTA HÄR
+        pass
+    
+    def show_menu(self):
+        while True:
+            print("...")
