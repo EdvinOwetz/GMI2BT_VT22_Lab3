2b914400b4fccea4e657f02c29fd281df40f3767
EdvinOwetz <edvinowetz@gmail.com>
2022-02-03 16:03:18 +0100

    Continued development. Added files and features, also removed another file deemd unnecessary.
    Co-authored-by: Nisslett <Nisslett@users.noreply.github.com>

--- modules.py
+++ modules.py
@@ -1,2 +1,37 @@
+from movie import Movie
 # A file containing methods and functions
 
+# Sök på film
+
+# Visa sökhistorik (senaste)
+    # Returnera en lista vari användaren kan välja index och visa mer info om vald film
+
+
+
+def movie_search():
+    #söker
+    #visar resultat (flera movies)
+    #väljer
+    #söker på id
+    #lägger till movie i historik
+    #visar resultat (en movie)
+    pass
+
+def show_history():
+    #visar sökhistorik
+    #väljer
+    #söker på id
+    #visar resultat (en movie)
+    pass
+
+def choose_movie(movie_list:list[Movie]) -> Movie:
+    #tar in en lista med movies 
+    #väljen
+    #returnerna den 
+    pass
+
+def show_movie(movie:Movie) -> None:
+    #sök på id
+    #Movie objectet.get_details()
+    #vsia resultat på lämpligt sätt?
+    pass
